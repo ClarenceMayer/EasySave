9b856060eb6cd1a3740d9881f3d840d928796aa2
Clarence <33149003+crunchi@users.noreply.github.com>
2020-05-09 05:59:18 +0200

    set up

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using EasySave.Controller;
+
+namespace EasySave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainController controller = new MainController();
+            controller.Run();
+            
+            // Console.Read();
+        }
+    }
+}
